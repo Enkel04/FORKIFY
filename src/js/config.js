c99bd94bbabd4a412dc7266939d86d3aaefda950
Enkel04 <xhelollarienkel@gmail.com>
2021-09-28 17:11:28 +0200

    vvfvvg

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '8fe5c351-16c9-4041-ab3c-08a1140051ed';
+export const MODAL_CLOSE_SEC = 2.5;
